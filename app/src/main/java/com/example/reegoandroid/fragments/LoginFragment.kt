0bace95baa320f9c6fa5a65b40f6a0b1b412a020
Alex Teper <ateper@fi.uba.ar>
2022-10-05 20:02:20 -0300

    Vista login con patron ViewModel y configuracion basica navigation

---
+++ app/src/main/java/com/example/reegoandroid/fragments/LoginFragment.kt
@@ -0,0 +1,50 @@
+package com.example.reegoandroid.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.viewModels
+import com.example.reegoandroid.R
+import com.example.reegoandroid.viewmodels.LoginViewModel
+
+class LoginFragment : Fragment() {
+    lateinit var v: View
+    private lateinit var txtLoginTitulo : TextView
+    private lateinit var btnLogin: Button
+
+    companion object {
+        fun newInstance() = LoginFragment()
+    }
+
+   private val loginViewModel : LoginViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        v = inflater.inflate(R.layout.fragment_login, container, false)
+        //findViewByid y codigo
+        txtLoginTitulo = v.findViewById(R.id.txtLoginTitle)
+        btnLogin = v.findViewById(R.id.btnLogin)
+
+        return v
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        txtLoginTitulo.text = loginViewModel.tituloLogin
+
+        btnLogin.setOnClickListener{
+            loginViewModel.iniciarSesion()
+            txtLoginTitulo.text = loginViewModel.tituloLogin
+        }
+    }
+
+
+}
