c7d966ec520b4db6adb0ae5735131af884cbb947
Santiago <santiago.santamaria@gmail.com>
2022-10-27 18:57:24 -0300

    request to local api endpoint with params via query ok

---
+++ app/src/main/java/com/example/reegoandroid/viewmodels/fiware/SensorRequestBody.kt
@@ -0,0 +1,13 @@
+package com.example.reegoandroid.viewmodels.fiware
+
+import com.squareup.moshi.Json
+
+data class SensorRequestBody(
+    @Json(name = "entityId")
+    val entityId: String,
+    @Json(name = "serviceHeader")
+    val serviceHeader: String,
+    @Json(name = "servicePathHeader")
+    val servicePathHeader: String,
+)
+
