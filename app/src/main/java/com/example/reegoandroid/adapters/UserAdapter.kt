48152e0f9a93a340885a83c64a9845c7cb51d85d
Alex Teper <ateper@fi.uba.ar>
2022-10-31 20:26:13 -0300

    Onclick user item without params

--- app/src/main/java/com/example/reegoandroid/adapters/UserAdapter.kt
+++ app/src/main/java/com/example/reegoandroid/adapters/UserAdapter.kt
@@ -4,11 +4,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.reegoandroid.R
 import com.example.reegoandroid.viewmodels.node.UserData
 
-class UserAdapter(var userList: MutableList<UserData>) : RecyclerView.Adapter<UserAdapter.UserHolder>() {
+class UserAdapter(var userList: MutableList<UserData>,  var onClick : (Int) -> Unit) : RecyclerView.Adapter<UserAdapter.UserHolder>() {
 
     class UserHolder (v: View) : RecyclerView.ViewHolder(v) {
 
@@ -25,6 +26,12 @@ class UserAdapter(var userList: MutableList<UserData>) : RecyclerView.Adapter<Us
             var userEmailView : TextView = view.findViewById(R.id.txtUserEmail)
             userEmailView.text = email
         }
+
+        fun getCard() : CardView {
+            return view.findViewById(R.id.card)
+        }
+
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserHolder {
@@ -38,6 +45,10 @@ class UserAdapter(var userList: MutableList<UserData>) : RecyclerView.Adapter<Us
             userList[i].name,
             userList[i].email,
         )
+
+        holder.getCard().setOnClickListener{
+            onClick(i)
+        }
     }
 
     override fun getItemCount(): Int {
