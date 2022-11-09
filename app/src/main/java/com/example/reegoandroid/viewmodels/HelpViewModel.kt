cef3b5c5514afa3d3c21cd4774771dac1424f1c6
Alex Teper <ateper@fi.uba.ar>
2022-11-09 01:11:01 -0300

    fix text help

--- app/src/main/java/com/example/reegoandroid/viewmodels/HelpViewModel.kt
+++ app/src/main/java/com/example/reegoandroid/viewmodels/HelpViewModel.kt
@@ -5,5 +5,5 @@ import androidx.lifecycle.ViewModel
 class HelpViewModel : ViewModel() {
 
    var titleText = "Bienvenido a Reego"
-   var textHelp = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum commodo interdum tortor, nec interdum nulla aliquet quis. Donec nibh quam, viverra nec lectus ut, cursus maximus ipsum. Phasellus molestie, ex id consequat porttitor, nibh velit posuere justo, a efficitur nunc libero mollis lectus. Sed vitae sapien volutpat, auctor dolor vitae, maximus quam. Maecenas eu felis accumsan, dignissim eros in, malesuada odio. Nulla cursus, nulla vel consequat scelerisque, magna sapien pharetra nisl, quis consequat nibh leo quis eros. Praesent nec pretium dolor, et eleifend risus. Donec hendrerit euismod pharetra. Praesent consectetur, velit eu bibendum sagittis, urna tellus viverra sem, a lacinia magna nunc a diam. Aenean finibus tellus et lorem tincidunt placerat. Nulla facilisi. Quisque sed scelerisque risus, quis dapibus "
+   var textHelp = "Bienvenido a la aplicacion Reego. Muchas gracias por confiar en nosotros para mantener sus cultivos hidratados y a salvo. En caso de querer recibir soporte para su sistema y/o iniciar en este mundo del riego automatizado por favor contactenos a soporte@reego.com "
 }
