c89203dc9b07bc7acce8a6537a61e17586a464c1
Alex Teper <ateper@fi.uba.ar>
2022-11-08 22:52:24 -0300

    navigation BackofficeFragment.kt

--- app/src/main/java/com/example/reegoandroid/fragments/BackofficeFragment.kt
+++ app/src/main/java/com/example/reegoandroid/fragments/BackofficeFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
 import com.example.reegoandroid.R
 import com.example.reegoandroid.viewmodels.BackofficeViewModel
 import com.example.reegoandroid.viewmodels.HelpViewModel
@@ -15,6 +17,7 @@ import com.example.reegoandroid.viewmodels.HelpViewModel
 class BackofficeFragment : Fragment() {
     lateinit var v: View
     private lateinit var txtTitle : TextView
+    private lateinit var btnUserList : Button
 
     private val backofficeViewModel : BackofficeViewModel by viewModels()
 
@@ -24,6 +27,7 @@ class BackofficeFragment : Fragment() {
     ): View? {
         v = inflater.inflate(R.layout.fragment_backoffice, container, false)
 
+        btnUserList = v.findViewById(R.id.btnUserList)
         txtTitle = v.findViewById(R.id.txtBackofficeTitle)
         return v
     }
@@ -32,6 +36,11 @@ class BackofficeFragment : Fragment() {
         super.onStart()
 
        txtTitle.text = "ReeGo"
+
+        btnUserList.setOnClickListener{
+            v.findNavController().navigate(BackofficeFragmentDirections.actionBackofficeFragmentToUserListFragment2())
+        }
+
     }
 
 
